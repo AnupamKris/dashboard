4550d1deeabd115a4746975d56c34bc71275c689
AnupamKris <anupamkris13262@gmail.com>
2024-10-06 23:25:15 +0530

    add: separator component

---
+++ src/views/components/Separator.vue
@@ -0,0 +1,23 @@
+<template>
+  <span
+    class="bg-border"
+    :class="
+      cn(orientation === 'vertical' ? 'mx-2 w-[1px] min-w-[1px]' : 'my-2 h-[1px] min-h-[1px]', props.class)
+    "></span>
+</template>
+
+<script setup>
+import { cn } from '@/lib/utils'
+const props = defineProps({
+  orientation: {
+    type: String,
+    default: 'horizontal'
+  },
+  class: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
