4cc637532fe5f6fff0f81f13106c1fc45ff1f771
AnupamKris <anupamkris13262@gmail.com>
2024-10-11 02:37:03 +0530

    feat: add count to tabs

--- src/views/components/PageBar.vue
+++ src/views/components/PageBar.vue
@@ -2,26 +2,31 @@
   <div class="flex w-full flex-col gap-3 space-y-2 border-b bg-background px-4 pt-4 dark:bg-pri/5">
     <div class="flex justify-between">
       <div class="flex flex-col">
-        <p class="text-base font-medium">
+        <div class="text-base font-medium">
           <slot name="title"></slot>
-        </p>
-        <p class="text-sm text-muted-foreground">
+        </div>
+        <div class="text-sm text-muted-foreground">
           <slot name="subtitle"></slot>
-        </p>
+        </div>
       </div>
       <div class="flex flex-row gap-2">
         <slot name="buttons"></slot>
       </div>
     </div>
-    <div class="flex gap-3 text-xs text-muted-foreground">
-      <p
+    <div class="flex gap-3 text-sm text-muted-foreground">
+      <div
         v-for="tab in tabs"
         :key="tab.name"
-        class="py-3"
-        :class="{ active: currentTab === tab.name }"
-        @click="currentTab = tab.name">
-        {{ tab.name }}
-      </p>
+        class="flex items-center gap-2 py-3"
+        :class="{ active: currentTab === tab.path }"
+        @click="currentTab = tab.path">
+        <p>{{ tab.name }}</p>
+        <p
+          class="flex h-4 w-4 items-center justify-center rounded-full bg-pri text-xs text-background"
+          v-if="tab.count">
+          {{ tab.count }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
